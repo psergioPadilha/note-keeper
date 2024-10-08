9f9b099c7fa8cdd4c569c6ad1e5dce9c15a91df8
psergioPadilha <163341752+psergioPadilha@users.noreply.github.com>
2024-10-08 19:23:33 -0300

    Aula 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
+import { ShellComponent } from "./core/components/shell/shell.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ MatCardModule, MatButtonModule ],
+  imports: [ShellComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })

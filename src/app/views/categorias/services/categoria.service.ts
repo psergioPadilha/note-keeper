ba5226f6500efcebabed930b84efdda45ab4de7d
psergioPadilha <163341752+psergioPadilha@users.noreply.github.com>
2024-10-12 13:49:42 -0300

    Atualização imports

--- src/app/views/categorias/services/categoria.service.ts
+++ src/app/views/categorias/services/categoria.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
-import { environment } from '../../../../environments/environment';
 import { HttpClient } from '@angular/common/http';
 import { CadastroCategoria, CategoriaCriada, CategoriaEditada, CategoriaExcluida, DetalhesCategoria, EdicaoCategoria, ListagemCategoria } from '../models/categoria.models';
 import { Observable } from 'rxjs';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
--- src/app/views/notas/services/nota.service.ts
+++ src/app/views/notas/services/nota.service.ts
@@ -1,8 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../../../environments/environment';
 import { CadastroNota, DetalhesNota, EdicaoNota, ListagemNota, NotaCriada, NotaEditada, NotaExcluida } from '../models/nota.models';
+import { environment } from '../../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
